49465c7cb496d8a40aa0eb4fc75dba6d101cdb3d
Philipp Tichy <philipptichy@hotmail.com>
2023-04-12 08:23:27 +0200

    push after about1

--- dist/main.js
+++ dist/main.js
@@ -65,3 +65,5 @@ function onToggleMenu(e){
     e.name = e.name === 'menu' ? 'close' : 'menu'
     navLinks.classList.toggle('top-[9%]')
 }
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,28 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'hero-pattern': "url('/assets/bild1.jpg')"
+        'hero-pattern': "url('/assets/bild1.jpg')",
+        'hero-about': "url('/assets/bild3.jpg')"
     },
     height: {
-      '60vh': '60vh',
-    }
-  },
+      '100vh': '100vh',
+    },
+    keyframes: {
+      wave: {
+        '0%': { transform: 'rotate(0.0deg)' },
+        '10%': { transform: 'rotate(14deg)' },
+        '20%': { transform: 'rotate(-8deg)' },
+        '30%': { transform: 'rotate(14deg)' },
+        '40%': { transform: 'rotate(-4deg)' },
+        '50%': { transform: 'rotate(10.0deg)' },
+        '60%': { transform: 'rotate(0.0deg)' },
+        '100%': { transform: 'rotate(0.0deg)' },
+      },
+    },
+    animation: {
+    'waving-hand': 'wave 2s linear infinite',
+    },
   plugins: [],
 }
 
-}
+}}
